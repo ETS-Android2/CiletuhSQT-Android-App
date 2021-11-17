1b11902a6c155608618489dc13bdcf9b6ec9a706
Arbi29 <arbipurba12@gmail.com>
2021-11-17 08:15:19 +0700

    Ticket detail
    Bagian 8, Video 2

--- app/src/main/java/com/example/sqoutgeo/HomeScreenAct.java
+++ app/src/main/java/com/example/sqoutgeo/HomeScreenAct.java
@@ -2,13 +2,28 @@ package com.example.sqoutgeo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class HomeScreenAct extends AppCompatActivity {
+
+    LinearLayout btn_ticket_aminities;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        btn_ticket_aminities = findViewById(R.id.btn_ticket_aminities);
+
+        btn_ticket_aminities.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoaminitiesticket  = new Intent(HomeScreenAct.this,TicketDetailAct.class);
+                startActivity(gotoaminitiesticket);
+            }
+        });
     }
 }
