3334c32cb965f1bd79b911bd626dc66037154dad
Aldree <aldri.ritonga77@gmail.com>
2021-11-17 22:56:15 +0700

    Profile Menu
    Halaman Utama profile user

--- app/src/main/java/com/example/sqoutgeo/HomeScreenAct.java
+++ app/src/main/java/com/example/sqoutgeo/HomeScreenAct.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class HomeScreenAct extends AppCompatActivity {
 
     LinearLayout btn_ticket_aminities;
+    ImageView btn_to_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,15 @@ public class HomeScreenAct extends AppCompatActivity {
         setContentView(R.layout.activity_home_screen);
 
         btn_ticket_aminities = findViewById(R.id.btn_ticket_aminities);
+        btn_to_profile = findViewById(R.id.btn_to_profile);
+
+        btn_to_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoprofile  = new Intent(HomeScreenAct.this,MyProfileAct.class);
+                startActivity(gotoprofile);
+            }
+        });
 
         btn_ticket_aminities.setOnClickListener(new View.OnClickListener() {
             @Override
