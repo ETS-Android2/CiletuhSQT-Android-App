f2bf0ec9bb4086c2734a03c14aec66dc714db05f
Arbi29 <arbipurba12@gmail.com>
2021-11-24 20:50:40 +0700

    RegisterTwo Error
    Video 12, bagian 2 awal

--- app/src/main/java/com/example/sqoutgeo/RegisterTwoAct.java
+++ app/src/main/java/com/example/sqoutgeo/RegisterTwoAct.java
@@ -3,15 +3,22 @@ package com.example.sqoutgeo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class RegisterTwoAct extends AppCompatActivity {
 
     LinearLayout btn_back;
-    Button btn_continue;
+    Button btn_continue, btn_add_photo;
+    ImageView pic_photo_register_user;
+
+    Uri photo_location;
+    Integer photo_max = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,18 @@ public class RegisterTwoAct extends AppCompatActivity {
 
 
         btn_continue = findViewById(R.id.btn_continue);
+        btn_back=findViewById(R.id.btn_back);
+        btn_add_photo = findViewById(R.id.btn_add_photo);
+        pic_photo_register_user = findViewById(R.id.pic_photo_register_user);
+
+
+        btn_add_photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              findPhoto();
+            }
+        });
+
         btn_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,7 +48,6 @@ public class RegisterTwoAct extends AppCompatActivity {
             }
         });
 
-        btn_back=findViewById(R.id.btn_back);
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,4 +56,14 @@ public class RegisterTwoAct extends AppCompatActivity {
             }
         });
     }
+
+    public void  findPhoto() {
+        Intent pic = new Intent();
+        pic.setType("image/*");
+        pic.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(pic, photo_max);
+    }
+
+
+
 }
