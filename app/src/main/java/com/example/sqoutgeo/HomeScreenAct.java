e4b493fd7a986aca35f140bd174aa0ded24aaaca
Aldree <aldri.ritonga77@gmail.com>
2022-01-04 22:15:07 +0700

    Amenities button
    untuk amenities

--- app/src/main/java/com/example/sqoutgeo/HomeScreenAct.java
+++ app/src/main/java/com/example/sqoutgeo/HomeScreenAct.java
@@ -3,6 +3,7 @@ package com.example.sqoutgeo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -37,5 +38,10 @@ public class HomeScreenAct extends AppCompatActivity {
                 startActivity(gotoaminitiesticket);
             }
         });
+    }
+
+    public void browser1(View view){
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse ( "https://storymaps.arcgis.com/stories/175df7ea77a74090965681bc3d0b8255" ));
+        startActivity ( browserIntent );
     }
 }
