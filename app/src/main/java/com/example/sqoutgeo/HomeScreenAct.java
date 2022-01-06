5d9bf170cd87dda634bd924729ced08b44cacfb5
Aldree <aldri.ritonga77@gmail.com>
2022-01-06 09:35:39 +0700

    all pages feature
    penambahan link mengarah kepada hub untuk semua fitur halaman

--- app/src/main/java/com/example/sqoutgeo/HomeScreenAct.java
+++ app/src/main/java/com/example/sqoutgeo/HomeScreenAct.java
@@ -41,7 +41,17 @@ public class HomeScreenAct extends AppCompatActivity {
     }
 
     public void browser1(View view){
-        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse ( "https://storymaps.arcgis.com/stories/175df7ea77a74090965681bc3d0b8255" ));
+        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse ( "https://arcg.is/0uLLGj" ));
         startActivity ( browserIntent );
+    }
+
+    public void penginapan(View view){
+        Intent penginapanIntent=new Intent (Intent.ACTION_VIEW,Uri.parse ( "https://arcg.is/ynaaK" ));
+        startActivity ( penginapanIntent );
+    }
+
+    public void wisata(View view){
+        Intent wisataIntent=new Intent (Intent.ACTION_VIEW,Uri.parse ( "https://arcg.is/HyHa5" ));
+        startActivity ( wisataIntent );
     }
 }
