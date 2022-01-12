1753866a345acfd255b8c7e5aa8a28facc162476
Arsyi-sul10 <sulthoniarsyii@gmail.com>
2022-01-12 21:10:05 +0700

    last update

---
+++ app/src/main/java/com/example/sqoutgeo/LocationListAct.java
@@ -0,0 +1,14 @@
+package com.example.sqoutgeo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationListAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_list);
+    }
+}
