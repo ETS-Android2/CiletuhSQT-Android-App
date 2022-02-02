1f96846d39909f2a023916694176117006f3688a
Arsyi-sul10 <sulthoniarsyii@gmail.com>
2022-02-02 22:40:49 +0700

    latest update

--- app/src/main/java/com/example/sqoutgeo/AboutUsAct.java
+++ app/src/main/java/com/example/sqoutgeo/AboutUsAct.java
@@ -3,12 +3,15 @@ package com.example.sqoutgeo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class AboutUsAct extends AppCompatActivity {
     LinearLayout btn_back;
+    Button aboutus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,16 @@ public class AboutUsAct extends AppCompatActivity {
             public void onClick(View view) {
                 Intent backtomenu  = new Intent(AboutUsAct.this,HomeScreenAct.class);
                 startActivity(backtomenu);
+            }
+        });
+
+        aboutus = findViewById(R.id.about_app);
+        aboutus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotostorymap = new Intent("android.intent.action.VIEW",
+                        Uri.parse("https://storymaps.arcgis.com/stories/a3d6543ad95649a2938f051ca9ea0b8d"));
+                startActivity(gotostorymap);
             }
         });
     }
